5b20f897d106633b3c1b55699986e8eb34ddb358
Alexey Gubin <ominator@ya.ru>
2022-05-11 23:43:51 +0300

    Solution

--- task02-slice.go
+++ task02-slice.go
@@ -1,16 +1,9 @@
 package homework
 
-import "sort"
-
-type Int64Slice []int64
-
-func (x Int64Slice) Len() int           { return len(x) }
-func (x Int64Slice) Less(i, j int) bool { return i < j }
-func (x Int64Slice) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
-
 func reverse(input []int64) (result []int64) {
-	result = make([]int64, 0, len(input))
-	copy(result, input)
-	sort.Sort(sort.Reverse(Int64Slice(result)))
+	result = make([]int64, len(input))
+	for i := 0; i < len(input); i++ {
+		result[len(input)-i-1] = input[i]
+	}
 	return result
 }
